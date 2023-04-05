7ac83506ff779f495dbf39b1f1de68ec191b67ba
Jeremias <jeremias@pickcells.bio>
2023-04-05 13:20:14 -0300

    feat: add all files update

--- client.py
+++ client.py
@@ -1,17 +1,52 @@
 import requests
+import json
 
-url_base = 'http://localhost:8000'
+url_base = 'http://127.0.0.1:8000'
 
-def send_message(message: str() or [str()] , act=0):
-    url = url_base + '/route/1'
+def test_server():
+    url = url_base + '/test/'
     headers = {'Content-Type': 'application/json'}
     response = requests.get(url)
     
-    body_respose = {
-        "message": response.json(),
-        "status_code": response.status_code
+    print(response.json())
+
+def test_server_post():
+    url = url_base + '/test/'
+    headers = {'Content-Type': 'application/json'}
+    body = {
+        "name": "Pedro",
+        "age": 19,
     }
-    return body_respose
+    response = requests.post(url, json=body, headers=headers)
+    
+    print(response.json())
+
+def div(a, b):
+    url = url_base + '/div/'
+    headers = {'Content-Type': 'application/json'}
+    body = {
+        "value_a": a,
+        "value_c": 10
+    }
+    response = requests.post(url, json=body, headers=headers)
+
+    print(response.json())
+
+def send_package(message: str() or [str()]):
+    url = url_base + '/send/package/'
+    headers = {'Content-Type': 'application/json'}
+    body = {
+        "message": message,
+        "wanted": ["status", "response", "message_qtd"]
+    }
+    response = requests.post(url, json=body, headers=headers)
+    
+    
+    print(response.json())
 
 
-print(send_message(["Messagem"]))
+# print(send_message(["Messagem"]))
+
+# test_server_post()
+
+div(10, 3)
--- server.py
+++ server.py
@@ -17,15 +17,66 @@ app.add_middleware(
     TrustedHostMiddleware, allowed_hosts=["127.0.0.1", "*"]
 )
 
-@app.get("/route/1")
-def route_1():
+@app.get("/test/")
+def route_test():
     try:
         return JSONResponse(status_code=200, content={
-            "message": "route1"
+            "message": "Você está na rota teste",
+            "status_code": 200,
+            "flag": True
         })
     except ValueError as e:
         return JSONResponse(status_code=500, content={
-            "msg":"Não consigo acessar a rota - {}".format(e)
+            "message": "Você está na rota teste, porém ocorreu um erro",
+            "status_code": 500,
+            "flag": True
+        })
+
+@app.post("/test/")
+async def route_post_test(request: Request):
+    try:
+        body = await request.body()
+        body = json.loads(body)
+        return JSONResponse(status_code=200, content={
+            "message": "Você está na rota teste, do tipo post, com o body: {}".format(body),
+            "status_code": 200,
+            "flag": True
+        })
+    except ValueError as e:
+        return JSONResponse(status_code=500, content={
+            "message": "Você está na rota teste, porém ocorreu um erro",
+            "status_code": 500,
+            "flag": True
+        })
+
+@app.post("/div/")
+async def route_div(request: Request):
+    try:
+        body = await request.body()
+        body = json.loads(body)
+        caso_test = 0
+
+        if int('value_a' in body.keys()):
+            caso_test += 1
+        
+        if int('value_c' in body.keys()):
+            caso_test += 1
+        
+        if caso_test < 2:
+            raise ValueError("Pacote não está de acordo com o protocolo (regra)")
+
+        result = int(body["value_a"]) / int(body["value_c"])
+        
+        return JSONResponse(status_code=200, content={
+            "message": "O resultado da divisão é: {}".format(result),
+            "status_code": 200,
+            "flag": True
+        })
+    except ValueError as e:
+        return JSONResponse(status_code=500, content={
+            "message": "Você está na rota de divisão, porém ocorreu um erro: {}".format(e),
+            "status_code": 500,
+            "flag": True
         })
 
 @app.post("/route/2")
@@ -42,3 +93,4 @@ async def route_2(request: Request):
         return JSONResponse(status_code=500, content={
             "msg":"Ocorreu um erro: {}".format(e)
         })
+
